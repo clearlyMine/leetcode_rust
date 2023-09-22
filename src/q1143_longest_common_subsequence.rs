9f8eb4a1a926fdf0479cce6ea13fcd4a902efb14
clearly mine <clearlymine@pm.me>
2023-09-22 23:03:27 +0530

    add multiple solutions for 1143
    Do this to have a future reference and to benchmark the different algorithms

--- src/q1143_longest_common_subsequence.rs
+++ src/q1143_longest_common_subsequence.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
     let (len1, len2) = (text1.len(), text2.len());
     let (text1, text2, len2) = if len1 > len2 {
@@ -16,6 +17,60 @@ pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
         }
     }
     *memo.last().unwrap_or(&0)
+}
+
+#[allow(dead_code)]
+pub fn longest_common_subsequence_my_dp(text1: String, text2: String) -> i32 {
+    let (len1, len2) = (text1.len(), text2.len());
+    let (text1, text2, len2) = if len1 > len2 {
+        (text1, text2, len2)
+    } else {
+        (text2, text1, len1)
+    };
+    let mut memo: Vec<i32> = vec![0; len2 + 1];
+    for c in text1.bytes() {
+        let mut prc;
+        let mut pr = 0;
+        for (j, d) in text2.bytes().enumerate() {
+            prc = pr;
+            pr = memo[j + 1];
+            memo[j + 1] = if c == d { 1 + prc } else { memo[j].max(pr) };
+        }
+    }
+    *memo.last().unwrap_or(&0)
+}
+
+#[allow(dead_code)]
+pub fn longest_common_subsequence_basic(text1: String, text2: String) -> i32 {
+    let (len1, len2) = (text1.len(), text2.len());
+    let (text1, text2) = (text1.as_bytes(), text2.as_bytes());
+    let mut memo: Vec<Vec<i32>> = vec![vec![0; len2 + 1]; len1 + 1];
+    for i in 1..=len1 {
+        for j in 1..=len2 {
+            memo[i][j] = if text1[i - 1] == text2[j - 1] {
+                1 + memo[i - 1][j - 1]
+            } else {
+                memo[i - 1][j].max(memo[i][j - 1])
+            };
+        }
+    }
+    memo[len1][len2]
+}
+
+#[allow(dead_code)]
+pub fn longest_common_subsequence_basic_2(text1: String, text2: String) -> i32 {
+    let (len1, len2) = (text1.len(), text2.len());
+    let mut memo: Vec<Vec<i32>> = vec![vec![0; len2 + 1]; len1 + 1];
+    for i in 1..=len1 {
+        for j in 1..=len2 {
+            memo[i][j] = if text1.chars().nth(i - 1) == text2.chars().nth(j - 1) {
+                1 + memo[i - 1][j - 1]
+            } else {
+                memo[i - 1][j].max(memo[i][j - 1])
+            };
+        }
+    }
+    memo[len1][len2]
 }
 
 #[cfg(test)]
@@ -39,6 +94,18 @@ mod tests {
         io.into_iter().for_each(|(input, output)| {
             assert_eq!(
                 longest_common_subsequence(input.0.to_string(), input.1.to_string()),
+                output
+            );
+            assert_eq!(
+                longest_common_subsequence_my_dp(input.0.to_string(), input.1.to_string()),
+                output
+            );
+            assert_eq!(
+                longest_common_subsequence_basic(input.0.to_string(), input.1.to_string()),
+                output
+            );
+            assert_eq!(
+                longest_common_subsequence_basic_2(input.0.to_string(), input.1.to_string()),
                 output
             );
         });
