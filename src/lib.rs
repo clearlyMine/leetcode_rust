4551752ddadc01e86912edbca56e257db47adb58
clearly mine <clearlymine@pm.me>
2023-09-23 11:46:05 +0530

    restructure problem 242

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,5 @@ pub mod p0198_house_robber;
 pub mod p0213_house_robber_ii;
 pub mod p0217_contain_duplicates;
 pub mod p0238_product_of_array_except_self;
+pub mod p0242_valid_anagram;
 pub mod q1143_longest_common_subsequence;
