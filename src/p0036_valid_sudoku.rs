b23f9b6c0f449d17e7b6703cdf1aa7a167eee53e
clearly mine <clearlymine@pm.me>
2023-09-25 17:47:09 +0530

    optimize solution for problem 0036

--- src/p0036_valid_sudoku.rs
+++ src/p0036_valid_sudoku.rs
@@ -1,68 +1,57 @@
 use std::collections::HashSet;
 
-pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
-    let mut cur_row: HashSet<char>;
-    for row in &board {
-        cur_row = HashSet::new();
-        for elem in row {
-            if elem == &'.' {
-                continue;
-            }
-            if cur_row.contains(elem) {
-                return false;
-            }
-            cur_row.insert(*elem);
-        }
-    }
-    let mut cur_col: HashSet<char>;
-    for j in 0..board[0].len() {
-        cur_col = HashSet::new();
-        for i in 0..board.len() {
-            let elem = board[i][j];
-            if elem == '.' {
-                continue;
-            }
-            if cur_col.contains(&elem) {
-                return false;
-            }
-            cur_col.insert(elem);
-        }
-    }
+//https://leetcode.com/problems/valid-sudoku/solutions/1261934/rust-solution-using-three-1-d-arrays-with-bit-manipulation-instead-of-hashset/
+pub fn is_valid_sudoku_new(board: Vec<Vec<char>>) -> bool {
+    let mut rows: [u16; 9] = [0; 9];
+    let mut cols: [u16; 9] = [0; 9];
+    let mut boxes: [u16; 9] = [0; 9];
 
-    fn get_all_sudoku_sections() -> [[(usize, usize); 9]; 9] {
-        let set = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
-        let mut out = [[(0, 0); 9]; 9];
-        let mut num = 0;
-        for set1 in &set {
-            for set2 in &set {
-                let mut o = [(0, 0); 9];
-                let mut n = 0;
-                for i in set1 {
-                    for j in set2 {
-                        o[n] = (*i, *j);
-                        n += 1;
+    for i in 0..9 {
+        for j in 0..9 {
+            match board[i][j] {
+                '.' => continue,
+                c => {
+                    let b: usize = (i / 3) * 3 + (j / 3);
+                    let curr = 1 << (c.to_digit(10).unwrap());
+
+                    if rows[i] & curr != 0 || cols[j] & curr != 0 || boxes[b] & curr != 0 {
+                        return false;
                     }
+
+                    rows[i] |= curr;
+                    cols[j] |= curr;
+                    boxes[b] |= curr;
                 }
-                out[num] = o;
-                num += 1;
             }
         }
-        out
     }
 
-    let mut cur_section: HashSet<char> = HashSet::new();
-    for section in get_all_sudoku_sections() {
-        for (i, j) in section {
-            let elem = board[i][j];
-            if elem == '.' {
-                continue;
-            }
-            if cur_section.contains(&elem) {
+    true
+}
+
+pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
+    for i in 0..9 {
+        let mut cur_row: HashSet<char> = HashSet::new();
+        let mut cur_col: HashSet<char> = HashSet::new();
+        let mut cur_section: HashSet<char> = HashSet::new();
+        for j in 0..9 {
+            let mut elem = board[i][j];
+            if elem != '.' && !cur_row.insert(elem) {
                 return false;
             }
-            cur_section.insert(elem);
+            elem = board[j][i];
+
+            if elem != '.' && !cur_col.insert(elem) {
+                return false;
+            }
+
+            let row_index = 3 * (i / 3);
+            let col_index = 3 * (i % 3);
+            elem = board[row_index + (j / 3)][col_index + (j % 3)];
+            if elem != '.' && !cur_section.insert(elem) {
+                return false;
+            }
         }
-        cur_section = HashSet::new();
     }
 
     true
@@ -112,5 +101,47 @@ mod tests {
             vec!['.', '.', '4', '.', '.', '.', '.', '.', '.'],
         ];
         assert!(!is_valid_sudoku(board));
+    }
+
+    #[test]
+    fn test_is_valid_sudoku_new() {
+        let board = vec![
+            vec!['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+            vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+            vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+            vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+            vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+            vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+            vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+            vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+            vec!['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+        ];
+        assert!(is_valid_sudoku_new(board));
+
+        let board = vec![
+            vec!['8', '3', '.', '.', '7', '.', '.', '.', '.'],
+            vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+            vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+            vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+            vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+            vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+            vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+            vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+            vec!['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+        ];
+        assert!(!is_valid_sudoku_new(board));
+
+        let board = vec![
+            vec!['.', '.', '.', '.', '5', '.', '.', '1', '.'],
+            vec!['.', '4', '.', '3', '.', '.', '.', '.', '.'],
+            vec!['.', '.', '.', '.', '.', '3', '.', '.', '1'],
+            vec!['8', '.', '.', '.', '.', '.', '.', '2', '.'],
+            vec!['.', '.', '2', '.', '7', '.', '.', '.', '.'],
+            vec!['.', '1', '5', '.', '.', '.', '.', '.', '.'],
+            vec!['.', '.', '.', '.', '.', '2', '.', '.', '.'],
+            vec!['.', '2', '.', '9', '.', '.', '.', '.', '.'],
+            vec!['.', '.', '4', '.', '.', '.', '.', '.', '.'],
+        ];
+        assert!(!is_valid_sudoku_new(board));
     }
 }
