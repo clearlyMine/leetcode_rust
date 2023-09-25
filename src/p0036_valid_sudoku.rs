ecb82126ebe61241c3a6d72014a58c73d34d5ac9
clearly mine <clearlymine@pm.me>
2023-09-25 14:06:54 +0530

    refactor solution for problem 0036

--- src/p0036_valid_sudoku.rs
+++ src/p0036_valid_sudoku.rs
@@ -29,36 +29,43 @@ pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
         }
     }
 
-    let mut cur_section: HashSet<char> = HashSet::new();
-    for set1 in vec![[0, 1, 2], [3, 4, 5], [6, 7, 8]] {
-        for set2 in vec![[0, 1, 2], [3, 4, 5], [6, 7, 8]] {
-            for (i, j) in get_all_combinations(set1, set2) {
-                let elem = board[i][j];
-                if elem == '.' {
-                    continue;
+    fn get_all_sudoku_sections() -> [[(usize, usize); 9]; 9] {
+        let set = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+        let mut out = [[(0, 0); 9]; 9];
+        let mut num = 0;
+        for set1 in &set {
+            for set2 in &set {
+                let mut o = [(0, 0); 9];
+                let mut n = 0;
+                for i in set1 {
+                    for j in set2 {
+                        o[n] = (*i, *j);
+                        n += 1;
+                    }
                 }
-                if cur_section.contains(&elem) {
-                    return false;
-                }
-                cur_section.insert(elem);
+                out[num] = o;
+                num += 1;
             }
-            cur_section = HashSet::new();
         }
+        out
+    }
+
+    let mut cur_section: HashSet<char> = HashSet::new();
+    for section in get_all_sudoku_sections() {
+        for (i, j) in section {
+            let elem = board[i][j];
+            if elem == '.' {
+                continue;
+            }
+            if cur_section.contains(&elem) {
+                return false;
+            }
+            cur_section.insert(elem);
+        }
+        cur_section = HashSet::new();
     }
 
     true
-}
-
-fn get_all_combinations(set1: [usize; 3], set2: [usize; 3]) -> [(usize, usize); 9] {
-    let mut out = [(0, 0); 9];
-    let mut num = 0;
-    for i in set1 {
-        for j in set2 {
-            out[num] = (i, j);
-            num += 1;
-        }
-    }
-    out
 }
 
 #[cfg(test)]
@@ -105,22 +112,5 @@ mod tests {
             vec!['.', '.', '4', '.', '.', '.', '.', '.', '.'],
         ];
         assert!(!is_valid_sudoku(board));
-    }
-
-    #[test]
-    fn test_get_all_combinations() {
-        let set = [0, 1, 2];
-        let out = [
-            (0, 0),
-            (0, 1),
-            (0, 2),
-            (1, 0),
-            (1, 1),
-            (1, 2),
-            (2, 0),
-            (2, 1),
-            (2, 2),
-        ];
-        assert_eq!(get_all_combinations(set, set), out);
     }
 }
