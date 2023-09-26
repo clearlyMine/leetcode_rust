9723cfca58c16bcecb7f7a2044185e3bb8aa1e46
clearly mine <clearlymine@pm.me>
2023-09-26 13:07:33 +0530

    optimize solution for problem 0091

--- src/p0091_decode_ways.rs
+++ src/p0091_decode_ways.rs
@@ -42,6 +42,50 @@ pub fn num_decodings(s: String) -> i32 {
     p2
 }
 
+pub fn num_decodings_bytes(s: String) -> i32 {
+    let len = s.len();
+
+    let chars = s.into_bytes();
+
+    let first = chars[0];
+    if first == b'0' {
+        //starts with zero
+        return 0;
+    }
+    if len == 1 {
+        return 1;
+    }
+
+    let mut p1 = 0;
+    let mut p2 = 0;
+    let mut tens = first;
+    let mut ones = chars[1];
+    if ones != b'0' {
+        p1 = 1;
+        p2 = 1;
+    }
+    if tens == b'1' || (tens == b'2' && ones < b'7') {
+        p2 += 1;
+    }
+    if len == 2 {
+        return p2;
+    }
+    for i in 2..len {
+        tens = ones;
+        ones = chars[i];
+        let mut x = 0;
+        if ones != b'0' {
+            x = p2;
+        }
+        if tens == b'1' || (tens == b'2' && ones < b'7') {
+            x += p1;
+        }
+        p1 = p2;
+        p2 = x;
+    }
+    p2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -53,5 +97,15 @@ mod tests {
         assert_eq!(num_decodings("27".to_string()), 1);
         assert_eq!(num_decodings("2101".to_string()), 1);
         assert_eq!(num_decodings("2611055971756562".to_string()), 4);
+    }
+
+    #[test]
+    fn test_num_decodings_bytes() {
+        assert_eq!(num_decodings_bytes("12".to_string()), 2);
+        assert_eq!(num_decodings_bytes("226".to_string()), 3);
+        assert_eq!(num_decodings_bytes("06".to_string()), 0);
+        assert_eq!(num_decodings_bytes("27".to_string()), 1);
+        assert_eq!(num_decodings_bytes("2101".to_string()), 1);
+        assert_eq!(num_decodings_bytes("2611055971756562".to_string()), 4);
     }
 }
